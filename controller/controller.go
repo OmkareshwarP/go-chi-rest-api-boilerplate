8a6f9d224c6153058ae591d4d0643ca87f592a3f
omkareshwar p <omece460@gmail.com>
2024-11-15 22:25:36 +0530

    get user and delete,update user apis added

--- controller/controller.go
+++ controller/controller.go
@@ -97,3 +97,69 @@ func UpdateUser(w http.ResponseWriter, r *http.Request) {
 	}
 	utils.ResponseGenerator(w, 404, true, "userNotFound", nil, "User does not exists.")
 }
+
+func DeleteUser(w http.ResponseWriter, r *http.Request) {
+	userId := chi.URLParam(r, "userId")
+
+	if userId == "" {
+		utils.ResponseGenerator(w, 403, true, "inputParamsValidationFailed", nil, "Some information missing while validating your request")
+		return
+	}
+
+	utils.Mu.Lock()
+	defer utils.Mu.Unlock()
+
+	for i, user := range utils.UsersData {
+		if user.UserID == userId {
+			utils.UsersData = append(utils.UsersData[:i], utils.UsersData[i+1:]...)
+			utils.ResponseGenerator(w, 200, false, "", nil, "User deleted successfully.")
+			return
+		}
+	}
+
+	utils.ResponseGenerator(w, 404, true, "userNotFound", nil, "User does not exists.")
+}
+
+func GetUserById(w http.ResponseWriter, r *http.Request) {
+	userId := chi.URLParam(r, "userId")
+
+	if userId == "" {
+		utils.ResponseGenerator(w, 403, true, "inputParamsValidationFailed", nil, "Some information missing while validating your request")
+		return
+	}
+
+	usersData := utils.UsersData
+
+	for _, user := range usersData {
+		if user.UserID == userId {
+			utils.ResponseGenerator(w, 200, false, "", map[string]interface{}{
+				"user": user,
+			}, "User details fetched successfully.")
+			return
+		}
+	}
+
+	utils.ResponseGenerator(w, 404, true, "userNotFound", nil, "User does not exists.")
+}
+
+func GetUserByUsername(w http.ResponseWriter, r *http.Request) {
+	username := chi.URLParam(r, "username")
+
+	if username == "" {
+		utils.ResponseGenerator(w, 403, true, "inputParamsValidationFailed", nil, "Some information missing while validating your request")
+		return
+	}
+
+	usersData := utils.UsersData
+
+	for _, user := range usersData {
+		if user.Username == username {
+			utils.ResponseGenerator(w, 200, false, "", map[string]interface{}{
+				"user": user,
+			}, "User details fetched successfully.")
+			return
+		}
+	}
+
+	utils.ResponseGenerator(w, 404, true, "userNotFound", nil, "User does not exists.")
+}
